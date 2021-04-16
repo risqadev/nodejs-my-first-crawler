f5b7e5b5efd55282072fef5dccb55129832e57e4
Ricardo Camacho <rscamachodev@gmail.com>
2021-04-16 13:16:37 -0300

    improve output files formating

--- index.js
+++ index.js
@@ -7,16 +7,28 @@ let count = 0;
 const crawl = async ({ url }) => {
   const response = await fetch(url);
   const html = await response.text();
+
   let $ = cheerio.load(html);
 
+  // cria o arquivo markdown
+  fs.writeFile('output.md', '', (err) => {
+    if (err) console.log(err);
+  });
+
+  // cria e escreve a primeira linha do arquivo csv
+  fs.writeFile('output.csv', 'Título,Data,Link,Texto introdução\n', (err) => {
+    if (err) console.log(err);
+  });
+
+  // coleta os artigos da página
   $('article').each((_i, article) => {
     let title = $(article).find('.article-title a').attr('title');
     let link = 'http://ppgedam.propesp.ufpa.br' + $(article).find('.article-title a').attr('href');
     let date = $(article).find('time').text().trim();
     let intro = $(article).find('.article-intro p').text().trim();
 
-    let markdown = `${title}\n###Data: ${date}\n###Link: [${link}](${link})\n\n${intro}\n\n\n`;
-    let csv = `"${title}","${date}","${link}","${intro}"\n`;
+    let markdown = `### ${title}\n\n**Data:** ${date}\n\n**Link:** [${link}](${link})\n\n${intro}\n\n\n\n`;
+    let csv = `'${title}',${date},${link},'${intro}'\n`;
 
     fs.appendFile('output.md', markdown, (err) => {
       if (err) console.log(err);
@@ -31,7 +43,15 @@ const crawl = async ({ url }) => {
 }
 
 const init = async () => {
-  for (let i = 18; i <= 138; i = i + 6) {
+
+  // // busca em uma única página
+  // await crawl({
+  //   url: `http://ppgedam.propesp.ufpa.br/index.php?start=6`
+  // });
+  // console.log(count);
+
+  // busca em um intervalo de páginas
+  for (let i = 0; i <= 18; i += 6) {
     await crawl({
       url: `http://ppgedam.propesp.ufpa.br/index.php?start=${i}`
     });
